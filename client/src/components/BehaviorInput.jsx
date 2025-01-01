cffdd2b25a4847fdad8874d357325f7ec0c20d30
frenzie24 <kevincharlesgross@gmail.com>
2025-01-01 06:03:41 -0600

    removed numberrange import from beahjviorinput componennt

--- client/src/components/BehaviorInput.jsx
+++ client/src/components/BehaviorInput.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import NumberRange from './numberRange';
+//import NumberRange from './numberRange';
 import NumberSelection from '../NumberSelection';
 
 
