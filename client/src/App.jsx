75da26a20fc7669137ab156479fb5e28fc266006
frenzie24 <kevincharlesgross@gmail.com>
2025-01-03 20:24:30 -0600

    set up initial game.jsx.  renamed old game.jsx to _game.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './Login';  // Import Login component
 import Registration from './Registration';  // Import Registration component
 import Profile from './Profile';  // Import UserProfile component
-import Game from "./Game";
+import Game from "./_Game";
 import GameSettings from './GameSettings';
 
 import Behavior from './Behavior';
@@ -15,12 +15,12 @@ const App = () => {
     <div className="w-screen h-screen bg-slate-900 text-slate-100">
       <Router>
         <Routes>
-          <Route path="/login" element={<GameSettings />} />
+          <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Registration />} />
           <Route path="/profile" element={<Profile />} />
           <Route path="/gamesettings" element={<GameSettings />} />
           <Route path="/game" element={<Game />} />
-          <Route path="/" element={<GameSettings />} />
+          <Route path="/" element={<Login />} />
         </Routes>
       </Router>
     </div>
